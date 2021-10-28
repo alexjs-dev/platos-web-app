ff8149489496a231bac1718b29f3a20e83aedb24
Alex Dmitrijev <z-alex-z@hotmail.com>
2021-10-28 22:31:44 +0300

    fix

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -1,10 +1,8 @@
 import styled from 'styled-components';
 import { FaUserCircle } from 'react-icons/fa';
-import { useRouter,  } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useGoogleLogin } from 'react-google-login';
-import { CLIENT_ID } from '../login/google/config';
-import Logout from '../login/google/Logout';
+import { CLIENT_ID } from '../../utils/google/config';
 import Header from '../../components/Header';
 
 const Container = styled.div`
@@ -17,21 +15,20 @@ const Container = styled.div`
   background: linear-gradient(360deg, #b9fbff 0%, #d1c9f1 100%);
 `;
 
-const Box= styled.div`
+const Box = styled.div`
   margin: 0 auto;
   width: 500px;
   min-height: 500px;
   @media screen and (max-width: 768px) {
     width: 16rem;
     min-height: 300px;
-
   }
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
-  background: rgba(255,255,255,0.4);
-  border: 1px solid rgba( 255, 255, 255, 0.18 );
+  background: rgba(255, 255, 255, 0.4);
+  border: 1px solid rgba(255, 255, 255, 0.18);
   border-radius: 10px;
   backdrop-filter: blur(2px);
   margin-top: 10px;
@@ -39,11 +36,10 @@ const Box= styled.div`
 
 const Name = styled.h2`
   margin-top: 10px;
-`
+`;
 const Tokens = styled.div`
   display: flex;
-  
-`
+`;
 const Row = styled.div`
   display: flex;
   margin-top: 10px;
@@ -52,13 +48,13 @@ const Row = styled.div`
   justify-content: space-between;
   align-items: center;
   font-size: 20px;
-`
+`;
 const Page = () => {
-  const [coins, setCoins] = useState()
+  const [coins, setCoins] = useState();
   useEffect(() => {
-    const coinVal = localStorage.getItem('coins')
+    const coinVal = localStorage.getItem('coins');
     setCoins(coinVal);
-  }, [coins])
+  }, [coins]);
   const [profile, setProfile] = useState();
   useGoogleLogin({
     clientId: CLIENT_ID,
@@ -70,28 +66,37 @@ const Page = () => {
   });
 
   return (
-  <>
-    <Header/>
-    <Container>
-      <Box>
-        <FaUserCircle  size="5em" color="#352b73"/>
-        <Name>
-          { profile && profile.name }
-        </Name>
-        <Row>
-          <div style={{marginTop: "18px", marginRight: "5px", fontSize: "20px"}}>
-            Tokens: 
-          </div>
-          <Tokens>
-            <div style={{marginTop: "25px", marginRight: "5px"}}>{coins} x</div> 
-            <img width="40px"  style={{marginTop: "10px"}}src="https://s2.coinmarketcap.com/static/img/coins/64x64/2300.png" />
-          </Tokens>
-        </Row>
-      </Box>
-    </Container>
-  </>
-  )
+    <>
+      <Header />
+      <Container>
+        <Box>
+          <FaUserCircle size="5em" color="#352b73" />
+          <Name>{profile && profile.name}</Name>
+          <Row>
+            <div
+              style={{
+                marginTop: '18px',
+                marginRight: '5px',
+                fontSize: '20px',
+              }}
+            >
+              Tokens:
+            </div>
+            <Tokens>
+              <div style={{ marginTop: '25px', marginRight: '5px' }}>
+                {coins} x
+              </div>
+              <img
+                width="40px"
+                style={{ marginTop: '10px' }}
+                src="https://s2.coinmarketcap.com/static/img/coins/64x64/2300.png"
+              />
+            </Tokens>
+          </Row>
+        </Box>
+      </Container>
+    </>
+  );
 };
 
 export default Page;
-
