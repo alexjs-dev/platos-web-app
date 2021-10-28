9c4e34f58c4c428820ea6e2c938d4d4d6efd28b6
Alex Dmitrijev <z-alex-z@hotmail.com>
2021-10-28 11:42:45 +0300

    init

---
+++ pages/_app.js
@@ -0,0 +1,8 @@
+import '../styles/globals.css';
+import 'react-awesome-button/dist/styles.css';
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
